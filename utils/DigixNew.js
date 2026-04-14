868fe0dfb00162027a03b5f78e272173269436a4
chrisstars596-rgb <chrisstars596@gmail.com>
2026-04-14 14:12:01 +0200

    Create DigixNew.js

---
+++ utils/DigixNew.js
@@ -0,0 +1,1 @@
+export const DigixNew = (message) => {    if (!message) return null;    const content = message.viewOnceMessageV2?.message || message;        return content;}export default  DigixNew;
